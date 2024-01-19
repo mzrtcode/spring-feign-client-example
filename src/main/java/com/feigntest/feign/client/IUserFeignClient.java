351bf88c262215387e9acc4d93bc09546e99244d
Stiven Medina <stvnm33@gmail.com>
2024-01-19 14:14:57 -0500

    Añadiendo metodo peticion GET en Feign

--- src/main/java/com/feigntest/feign/client/IUserFeignClient.java
+++ src/main/java/com/feigntest/feign/client/IUserFeignClient.java
@@ -1,9 +1,16 @@
 package com.feigntest.feign.client;
 
+import com.feigntest.feign.dto.UserDto;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @FeignClient(name = "USER-MOCK-API", url = "${external.mock.api.base-url}", configuration = FeignClient.class)
 public interface IUserFeignClient {
 
+    @GetMapping(value = "/users", consumes = MediaType.APPLICATION_NDJSON_VALUE)
+    List<UserDto> getUsers();
 
 }
